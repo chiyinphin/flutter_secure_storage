24bc22ca458a5cb5f4dd5a0b235fc7db4aa45e2a
Chi Yin Phin <chiyinphin@gmail.com>
2025-04-24 12:03:03 +0800

    Update StorageCipher18Implementation.java

--- flutter_secure_storage/android/src/main/java/com/it_nomads/fluttersecurestorage/ciphers/StorageCipher18Implementation.java
+++ flutter_secure_storage/android/src/main/java/com/it_nomads/fluttersecurestorage/ciphers/StorageCipher18Implementation.java
@@ -57,7 +57,9 @@ public class StorageCipher18Implementation implements StorageCipher {
     }
 
     protected Cipher getCipher() throws Exception {
-        return Cipher.getInstance("AES/CBC/PKCS7Padding");
+        return Cipher.getInstance("AES/GCM/NoPadding");
+  
+
     }
 
     @Override
